5f44df5bd9bba923badcf7e716b73179173d85bf
Roman <rspechenkin@gmail.com>
2020-01-17 20:36:28 +0000

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
